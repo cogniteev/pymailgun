45df118d1b04b4432bb00adc37cc606722106352
Philipp Schmitt <philipp@schmitt.co>
2017-02-07 08:38:27 +0100

    Python 3 compatibility

--- pymailgun/__init__.py
+++ pymailgun/__init__.py
@@ -2,5 +2,6 @@
 
 """
 
-from client import Client
-from client import MailgunCredentialsError
+from __future__ import absolute_import
+from .client import Client
+from .client import MailgunCredentialsError
