d01182948bd13d2febe33675bfc4b16767d40fc2
Valentin <valentin@oncrawl.com>
2021-04-27 10:37:11 +0200

    adding tags parameter to client

--- pymailgun/client.py
+++ pymailgun/client.py
@@ -24,7 +24,7 @@ class Client:
         return requests.request(method, url, data=data, auth=auth, files=files)
 
     def send_mail(self, sender, to, subject, text, html=None, cc=None, bcc=None,
-                  files=None, in_reply_to=None):
+                  files=None, in_reply_to=None, tags=None):
         """ Sends an email using the mailgun API
 
         @param sender: sender's email address
@@ -36,6 +36,7 @@ class Client:
         @param html: email's html content
         @param files: list of file paths to put in attachment
         @param in_reply_to: id of the mail to respond to
+        @param tags: tag(s) to apply to the email
         @return: the mailgun's API response
         """
         data = {'from': sender, 'to': to, 'subject': subject, 'text': text}
@@ -48,6 +49,8 @@ class Client:
             data['bcc'] = bcc
         if in_reply_to:
             data['h:In-Reply-To'] = in_reply_to
+        if tags:
+            data['o:tag'] = tags
 
         if files and isinstance(files, list):
             attached_files = []
